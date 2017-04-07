053bccea463920c81c45317df354618f415651e5
Sharku2011 <pjsw007@naver.com>
2017-04-08 01:08:15 +0900

    소스 업로드
    소스 업로드

---
+++ ML/Perceptron.cpp
@@ -0,0 +1,150 @@
+#include "Perceptron.h"
+#include <cmath>
+#include "../../../SharkuLibrary/GSL/span.h"
+
+double Perceptron::FeedForward(const vector<double> NewInput)
+{
+
+	double Sigma = 0.0;
+
+	Input = NewInput;
+
+	if (InputSize == NewInput.size())
+	{
+		for (size_t i = 0; i < InputSize; ++i)
+		{
+			Sigma += Weight[i] * Input[i];
+		}
+		Sigma += Bias;
+
+		Output = GetActivation(std::move(Sigma));
+
+		return Output;
+	}
+
+	return -1.0;
+}
+
+double Perceptron::GetActivation(double&& X)
+{
+	// for linear or identity activation function
+	return X;
+
+	// for ReLU activation function
+	//return MAX2(0.0, X);
+}
+
+double Perceptron::GetActivationGrad(double&& X, double(*pFunc)(const double&) = nullptr)
+{
+	if (pFunc)
+	{
+		return pFunc(X);
+	}
+	
+	return 1.0f;
+}
+
+double Perceptron::GetActivationGrad(double X, double(*pFunc)(double&&) = nullptr)
+{
+	if (pFunc)
+	{
+		return pFunc(std::move(X));
+	}
+
+	return 1.0f;
+}
+
+void Perceptron::PropBackward(const double& Target, const double& LearningRate /*= 0.1*/)
+{
+
+	const double Grad = (Output - Target) * GetActivationGrad(std::move(Output), &Perceptron::Sigmoid);
+
+	for (size_t i = 0; i < InputSize; ++i)
+	{
+		Weight[i] -= LearningRate * Grad;
+	}
+
+	Bias -= LearningRate * Grad * 1.0;
+	FeedForward(Input);
+}
+
+std::ostream & operator<<(std::ostream & c, const Perceptron& P)
+{
+	c << "Size : " << P.InputSize << endl;
+
+	c << "Input : [";
+	for (size_t i = 0; i < P.InputSize - 1; ++i)
+	{
+		c << P.Input[i] << ", ";
+	}
+	c << P.Input.back() << "]";
+
+	c << ", ";
+
+	c << "Output : " << P.Output << endl;
+
+
+	c << "Weight : [";
+	for (size_t i = 0; i < P.InputSize - 1; ++i)
+	{
+		c << P.Weight[i] << ", ";
+	}
+	c << P.Weight.back() << "]";
+
+	c << ", ";
+
+	c << "Bias : " << P.Bias;
+
+
+	return c;
+}
+
+int main()
+{
+	// 소수점 및 n번째 자리까지 출력
+	cout.precision(11);
+	
+	// 소수점 이하 자리의 0도 무시하지 않고 출력
+	cout.setf(ios::showpoint);
+
+	vector<double> Init_Weight, Init_Input;
+
+	Init_Weight.reserve(2);
+	Init_Weight.push_back(0.5);
+	Init_Weight.push_back(0.28);
+
+	Init_Input.reserve(2);
+	Init_Input.push_back(5.0);
+	Init_Input.push_back(7.0);
+
+	Perceptron MyPerceptron(Init_Weight, 1.0);	
+
+	MyPerceptron.FeedForward(Init_Input);
+	cout << MyPerceptron << endl;
+
+	double Target = 13.0;
+
+	for (int i = 0; i < 100; ++i)
+	{
+		std::cout << "\nTraining" << i+1 << std::endl;
+		MyPerceptron.PropBackward(std::move(Target));
+
+		cout << MyPerceptron << endl;
+
+		if (abs(MyPerceptron.Output - Target) < SMALL_NUMBER)
+		{
+			break;
+		}
+	}
+	
+	std::cout << "\nTraining Complete" << std::endl;
+	
+	cout << MyPerceptron << endl;
+
+	std::cout << std::endl;
+
+
+	gsl::span<int, 3>;
+	
+	return 0;
+}
---
+++ ML/Perceptron.h
@@ -0,0 +1,53 @@
+#pragma once
+#include <iostream>
+#include <vector>
+
+#define SMALL_NUMBER 1e-9
+#define MAX2(a,b) ((a) > (b) ? (a) : (b))
+
+using namespace std;
+
+class Perceptron
+{
+
+public:
+
+	size_t InputSize;
+	
+	vector<double> Weight; // weight of one input
+	double Bias;		// bias
+
+	vector<double> Input;
+	double Output;
+
+protected:
+
+private:
+
+public:
+
+	Perceptron() : InputSize(0), Weight(vector<double>()), Bias(0.0) {};
+	Perceptron(vector<double> NewWeight, double NewBias) : Weight(NewWeight), Bias(NewBias) { InputSize = NewWeight.size(); };
+
+	double FeedForward(const vector<double> NewInput);
+
+	double GetActivation(double&& X);
+
+	double GetActivationGrad(double&& X, double(*pFunc)(const double&));
+	double GetActivationGrad(double X, double(*pFunc)(double&&));
+
+	void PropBackward(const double& Target, const double& LearningRate = 0.1);
+	//void PropBackward(double&& Target, const double& LearningRate = 0.1);
+
+	static double Sigmoid(double&& x) { return 1 / (1 + exp(-x)); };
+	static double ReLU(double&& x) { return MAX2(0, x); };
+
+	friend ostream& operator<<(ostream& c, Perceptron&& P);
+
+protected:
+
+private:
+
+};
+
+
